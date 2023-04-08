10766604b53ac2d1b1c36d6333acfcf2544ffb55
ar.semenov <ar.semenov@qsoft.ru>
2023-04-08 21:07:20 +0300

    init front

---
+++ front/src/app/styles/basic.ts
@@ -0,0 +1,11 @@
+export const basic = {
+  '*, *::before, &::after': {
+    wordWrap: 'normal',
+  },
+  'html, body': {
+    minHeight: '100vh',
+  },
+  '.promo-banner': {
+    width: '100%',
+  },
+}
